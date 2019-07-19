7de273d5c3e4f3085686c78be30b10a5e5deebe6
pilets <pil717171@gmail.com>
2019-07-19 20:10:53 +0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return <nav className="nav">
+    <div className="item"> <a href="http://">Profile</a> </div>
+    <div className="item"> <a href="http://">Messages</a> </div>
+    <div className="item"> <a href="http://">News</a> </div>
+    <div className="item"> <a href="http://">Music</a> </div>
+    <div className="item"> <a href="http://">Setting</a> </div>
+  </nav>
+}
+
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className="content">
+    <div>
+      <img src="https://s1.1zoom.ru/big0/930/Coast_Sunrises_and_sunsets_Waves_USA_Ocean_Kaneohe_521540_1280x775.jpg" alt="" srcset=""/>
+    </div>
+    <div>
+      ava + description 
+    </div>
+    <div>
+      My post
+      <div> New post</div>
+      <div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile;
